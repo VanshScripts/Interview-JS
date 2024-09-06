1db6026686e0002e3ab79f3e901ffdaab938900b
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-09-06 15:09:38 +0000

    basics

--- lec1.js
+++ lec1.js
@@ -1,1 +1,33 @@
-console.log("Vansh")
+// console.log("Vansh")
+
+const accountId = 12323;
+let emailId = "vans72322@ghas00"
+var accountPassword = "21432430"
+accountCity = "haridwar"
+accountID = 2 //      Not allowed because of const variable
+//console.log(accountId);
+
+
+emailId = "vans72322@ghas00"
+accountPassword = "21432430"
+accountCity = "Jaipur"
+let accountState;  // without value the variable becomes  undefined
+ // console.table( [accountState ,emailId , accountPassword , accountCity]); // printing in tabular form in an array
+
+
+
+
+// Datatypes
+let name = "vansh" // string
+let age = 17 // number
+let isLoggedIn = false  //boolean
+
+
+//Objects
+// console.log(typeof "vansh"); // It tells type of datatype #string
+// console.log(typeof 13 ); // It tells type of datatype #number
+console.log(typeof null); //The value null was represented as a special value, but due to its binary representation (0), it was incorrectly identified as an object.
+console.log(typeof undefined);  // It tells type of datatype
+
+
+
